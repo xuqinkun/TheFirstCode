1f91a3a556a623798bbc94f019fd6631c32fa3ec
xuqinkun <xu_qinkun@163.com>
2025-05-22 22:04:57 +0800

    菜单栏点击事件

---
+++ app/src/main/java/com/xqk/helloworld/FirstActivity.java
@@ -0,0 +1,43 @@
+package com.xqk.helloworld;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FirstActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
+        Log.d(TAG, "onCreate: init");
+        Button btn = findViewById(R.id.btn1);
+//        btn.setOnClickListener(v -> Toast.makeText(MainActivity.this, "You clicked btn1", Toast.LENGTH_LONG).show());
+        btn.setOnClickListener(v -> finish());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == R.id.add_item) {
+            Toast.makeText(FirstActivity.this, "You clicked add item", Toast.LENGTH_SHORT).show();
+        } else if (item.getItemId() == R.id.remove_item) {
+            Toast.makeText(FirstActivity.this, "You clicked remove item", Toast.LENGTH_SHORT).show();
+        }
+        return true;
+    }
+}
