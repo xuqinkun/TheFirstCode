1deaa740ebdc7bbce48bb2467b494fb561971d32
xuqinkun <xu_qinkun@163.com>
2025-05-24 17:52:07 +0800

    《第一行代码》 第3章 UI 自定义布局，标题栏，添加点击事件

--- app/src/main/java/com/android/chapter03/TitleLayout.java
+++ app/src/main/java/com/android/chapter03/TitleLayout.java
@@ -1,9 +1,12 @@
 package com.android.chapter03;
 
+import android.app.Activity;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
@@ -11,5 +14,13 @@ public class TitleLayout extends LinearLayout {
     public TitleLayout(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         LayoutInflater.from(context).inflate(R.layout.title, this);
+        Button backBtn = findViewById(R.id.title_back);
+        Button editBtn = findViewById(R.id.title_edit);
+        backBtn.setOnClickListener(v -> {
+            ((Activity)getContext()).finish();
+        });
+        editBtn.setOnClickListener(v -> {
+            Toast.makeText(getContext(), "Edit", Toast.LENGTH_SHORT).show();
+        });
     }
 }
