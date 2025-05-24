d1566432ff971e67bf47adcf53151990fc662451
xuqinkun <xu_qinkun@163.com>
2025-05-24 16:42:51 +0800

    《第一行代码》 第3章 UI 对话框

--- app/src/main/java/com/android/chapter03/MainActivity.java
+++ app/src/main/java/com/android/chapter03/MainActivity.java
@@ -1,27 +1,39 @@
 package com.android.chapter03;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button btn = findViewById(R.id.button);
-        EditText text = findViewById(R.id.edit_text);
-        ImageView imageView = findViewById(R.id.image_view);
-        ProgressBar progressBar = findViewById(R.id.progress_bar);
         btn.setOnClickListener(v -> {
-            progressBar.setProgress(progressBar.getProgress() + 10);
+            AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+            dialog.setTitle("This is a dialog");
+            dialog.setMessage("FBI WARNING!");
+            dialog.setCancelable(false);
+            dialog.setPositiveButton("OK", (dialog1, which) -> {
+                Log.d(TAG, "It's ok");
+            });
+            dialog.setNegativeButton("Cancel", (dialog1, which) -> {
+                Log.d(TAG, "It's canceled");
+            });
+
+            dialog.show();
         });
     }
 }
