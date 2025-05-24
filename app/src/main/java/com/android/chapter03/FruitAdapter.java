5c50895a2a917a8ad2f85dd80aee18780780a9a7
xuqinkun <xu_qinkun@163.com>
2025-05-24 18:58:00 +0800

    《第一行代码》 第3章 UI ListView 效率优化

--- app/src/main/java/com/android/chapter03/FruitAdapter.java
+++ app/src/main/java/com/android/chapter03/FruitAdapter.java
@@ -14,7 +14,7 @@ import androidx.annotation.Nullable;
 import java.util.List;
 
 public class FruitAdapter extends ArrayAdapter<Fruit> {
-    private int resourceId;
+    private final int resourceId;
 
     public FruitAdapter(@NonNull Context context, int resource, @NonNull List<Fruit> objects) {
         super(context, resource, objects);
@@ -33,11 +33,25 @@ public class FruitAdapter extends ArrayAdapter<Fruit> {
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         Fruit fruit = getItem(position);
         // 第三个参数指定成false，表示只让我们在父布局中声明的layout属性生效，但不会为这个View添加父布局
-        View view = LayoutInflater.from(getContext()).inflate(resourceId, parent, false);
-        ImageView fruitImage = view.findViewById(R.id.fruit_image);
-        TextView fruitName = view.findViewById(R.id.fruit_name);
-        fruitImage.setImageResource(fruit.getImageId());
-        fruitName.setText(fruit.getName());
+        View view;
+        ViewHolder viewHolder;
+        if(convertView == null) {
+            view  = LayoutInflater.from(getContext()).inflate(resourceId, parent, false);
+            viewHolder = new ViewHolder();
+            viewHolder.fruitImage = view.findViewById(R.id.fruit_image);
+            viewHolder.fruitName = view.findViewById(R.id.fruit_name);;
+            view.setTag(viewHolder);
+        } else {
+            view = convertView;
+            viewHolder = (ViewHolder) view.getTag();
+        }
+        viewHolder.fruitImage.setImageResource(fruit.getImageId());
+        viewHolder.fruitName.setText(fruit.getName());
         return view;
+    }
+
+    class ViewHolder {
+        ImageView fruitImage;
+        TextView fruitName;
     }
 }
