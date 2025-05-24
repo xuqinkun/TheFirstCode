d98409799793ab3c8e6293af528d2c9f5d04d197
xuqinkun <xu_qinkun@163.com>
2025-05-24 10:23:45 +0800

    《第一行代码》 第2章 先从看得到的入手——探究活动 活动生命周期

---
+++ app/src/main/java/com/xqk/helloworld/MainActivity.java
@@ -0,0 +1,67 @@
+package com.xqk.helloworld;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
+        setContentView(R.layout.main_layout);
+        Button startNormal = findViewById(R.id.start_normal);
+        Button startDialog = findViewById(R.id.start_dialog);
+        startNormal.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, NormalActivity.class));
+        });
+        startDialog.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, DialogActivity.class));
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
+}
