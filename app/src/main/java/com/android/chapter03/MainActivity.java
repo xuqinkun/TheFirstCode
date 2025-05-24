acd6ef938d449a442a712af6d91a4e681b156597
xuqinkun <xu_qinkun@163.com>
2025-05-24 16:28:30 +0800

    《第一行代码》 第3章 UI ImageView控件

--- app/src/main/java/com/android/chapter03/MainActivity.java
+++ app/src/main/java/com/android/chapter03/MainActivity.java
@@ -3,6 +3,7 @@ package com.android.chapter03;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,9 +17,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Button btn = findViewById(R.id.button);
         EditText text = findViewById(R.id.edit_text);
+        ImageView imageView = findViewById(R.id.image_view);
         btn.setOnClickListener(v -> {
             String string = text.getText().toString();
             Toast.makeText(MainActivity.this, string, Toast.LENGTH_SHORT).show();
+            imageView.setImageResource(R.drawable.img_2);
         });
     }
 }
