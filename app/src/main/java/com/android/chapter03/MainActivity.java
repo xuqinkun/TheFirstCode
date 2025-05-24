4d64e31d4450f138f08cb0135b1dba9828e4a0be
xuqinkun <xu_qinkun@163.com>
2025-05-24 16:27:27 +0800

    《第一行代码》 第3章 UI EditText控件使用

--- app/src/main/java/com/android/chapter03/MainActivity.java
+++ app/src/main/java/com/android/chapter03/MainActivity.java
@@ -1,6 +1,9 @@
 package com.android.chapter03;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = findViewById(R.id.button);
+        EditText text = findViewById(R.id.edit_text);
+        btn.setOnClickListener(v -> {
+            String string = text.getText().toString();
+            Toast.makeText(MainActivity.this, string, Toast.LENGTH_SHORT).show();
+        });
     }
 }
