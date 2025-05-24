cb79a30b0df37726912533de4d5e3a2d3f57db72
xuqinkun <xu_qinkun@163.com>
2025-05-24 22:49:54 +0800

    《第一行代码》 第4章 探究碎片 碎片和活动之间进行通信

--- app/src/main/java/com/android/chapter04/LeftFragment.java
+++ app/src/main/java/com/android/chapter04/LeftFragment.java
@@ -4,18 +4,44 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import com.android.main.MainActivity;
 import com.android.main.R;
 
 
 public class LeftFragment extends Fragment {
+    private boolean isAnother = false;
+    private MainActivity activity;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.left_fragment, container, false);
+        activity = (MainActivity) getActivity();
+        View view = inflater.inflate(R.layout.left_fragment, container, false);
+        Button button = view.findViewById(R.id.button_replace);
+        button.setOnClickListener(v -> {
+            if (isAnother) {
+                replaceFragment(new RightFragment());
+                isAnother = false;
+            } else {
+                isAnother = true;
+                replaceFragment(new AnotherRightFragment());
+            }
+        });
+        return view;
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = activity.getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.right_layout, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
