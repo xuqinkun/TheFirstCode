0fda1d377a00c93364d89b67e138762ed23729e0
xuqinkun <xu_qinkun@163.com>
2025-05-24 17:48:17 +0800

    《第一行代码》 第3章 UI 自定义布局，标题栏

--- app/src/main/java/com/android/chapter03/LayoutActivity.java
+++ app/src/main/java/com/android/chapter03/LayoutActivity.java
@@ -3,12 +3,17 @@ package com.android.chapter03;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LayoutActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.percent_frame_layout);
+        setContentView(R.layout.activity_layout);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
     }
 }
