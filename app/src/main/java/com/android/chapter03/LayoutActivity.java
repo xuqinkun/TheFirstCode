a961dc06b676382bf783c84fc6120c560306e34c
xuqinkun <xu_qinkun@163.com>
2025-05-24 17:09:12 +0800

    《第一行代码》 第3章 UI 相对布局 相对父控件对齐

--- app/src/main/java/com/android/chapter03/LayoutActivity.java
+++ app/src/main/java/com/android/chapter03/LayoutActivity.java
@@ -9,6 +9,6 @@ public class LayoutActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_layout);
+        setContentView(R.layout.relative_layout);
     }
 }
