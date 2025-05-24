c4b628bc73176e434612c07b5499080a5f7eda49
xuqinkun <xu_qinkun@163.com>
2025-05-24 18:51:24 +0800

    《第一行代码》 第3章 UI ListView显示水果列表

---
+++ app/src/main/java/com/android/chapter03/FruitDisplayActivity.java
@@ -0,0 +1,48 @@
+package com.android.chapter03;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FruitDisplayActivity extends AppCompatActivity {
+    private List<Fruit> fruitList = new ArrayList<>();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.listview_layout);
+        initFruits();
+        FruitAdapter adapter = new FruitAdapter(FruitDisplayActivity.this, R.layout.fruit_item, fruitList);
+        ListView listView = findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
+    }
+        private void initFruits() {
+            for (int i = 0; i < 2; i++) {
+                Fruit apple = new Fruit("Apple", R.drawable.apple_pic);
+                fruitList.add(apple);
+                Fruit banana = new Fruit("Banana", R.drawable.banana_pic);
+                fruitList.add(banana);
+                Fruit orange = new Fruit("Orange", R.drawable.orange_pic);
+                fruitList.add(orange);
+                Fruit watermelon = new Fruit("Watermelon", R.drawable.watermelon_pic);
+                fruitList.add(watermelon);
+                Fruit pear = new Fruit("Pear", R.drawable.pear_pic);
+                fruitList.add(pear);
+                Fruit grape = new Fruit("Grape", R.drawable.grape_pic);
+                fruitList.add(grape);
+                Fruit pineapple = new Fruit("Pineapple", R.drawable.pineapple_pic);
+                fruitList.add(pineapple);
+                Fruit strawberry = new Fruit("Strawberry", R.drawable.strawberry_pic);
+                fruitList.add(strawberry);
+                Fruit cherry = new Fruit("Cherry", R.drawable.cherry_pic);
+                fruitList.add(cherry);
+                Fruit mango = new Fruit("Mango", R.drawable.mango_pic);
+                fruitList.add(mango);
+            }
+    }
+}
