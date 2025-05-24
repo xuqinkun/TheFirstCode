6c12dbe0e6e142b6efdf6a28318a7a970a68efb1
xuqinkun <xu_qinkun@163.com>
2025-05-24 17:27:33 +0800

    《第一行代码》 第3章 UI 百分比布局

--- app/src/main/java/com/android/chapter03/LayoutActivity.java
+++ app/src/main/java/com/android/chapter03/LayoutActivity.java
@@ -9,6 +9,6 @@ public class LayoutActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.frame_layout);
+        setContentView(R.layout.percent_frame_layout);
     }
 }
