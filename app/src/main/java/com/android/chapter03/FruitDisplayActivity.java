20301b520288b90dcf1c20d3d55a09384fd8517d
xuqinkun <xu_qinkun@163.com>
2025-05-24 19:43:38 +0800

    《第一行代码》 第3章 UI RecyclerView实现瀑布流布局

--- app/src/main/java/com/android/chapter03/FruitDisplayActivity.java
+++ app/src/main/java/com/android/chapter03/FruitDisplayActivity.java
@@ -1,18 +1,15 @@
 package com.android.chapter03;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class FruitDisplayActivity extends AppCompatActivity {
     private final List<Fruit> fruitList = new ArrayList<>();
@@ -23,33 +20,53 @@ public class FruitDisplayActivity extends AppCompatActivity {
         setContentView(R.layout.recycler_layout);
         initFruits();
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        StaggeredGridLayoutManager layoutManager = new StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(new RecyclerAdapter(fruitList));
     }
-        private void initFruits() {
-            for (int i = 0; i < 2; i++) {
-                Fruit apple = new Fruit("Apple", R.drawable.apple_pic);
-                fruitList.add(apple);
-                Fruit banana = new Fruit("Banana", R.drawable.banana_pic);
-                fruitList.add(banana);
-                Fruit orange = new Fruit("Orange", R.drawable.orange_pic);
-                fruitList.add(orange);
-                Fruit watermelon = new Fruit("Watermelon", R.drawable.watermelon_pic);
-                fruitList.add(watermelon);
-                Fruit pear = new Fruit("Pear", R.drawable.pear_pic);
-                fruitList.add(pear);
-                Fruit grape = new Fruit("Grape", R.drawable.grape_pic);
-                fruitList.add(grape);
-                Fruit pineapple = new Fruit("Pineapple", R.drawable.pineapple_pic);
-                fruitList.add(pineapple);
-                Fruit strawberry = new Fruit("Strawberry", R.drawable.strawberry_pic);
-                fruitList.add(strawberry);
-                Fruit cherry = new Fruit("Cherry", R.drawable.cherry_pic);
-                fruitList.add(cherry);
-                Fruit mango = new Fruit("Mango", R.drawable.mango_pic);
-                fruitList.add(mango);
-            }
+
+    private void initFruits() {
+        for (int i = 0; i < 2; i++) {
+            Fruit apple = new Fruit(
+                    getRandomLengthName("Apple"), R.drawable.apple_pic);
+            fruitList.add(apple);
+            Fruit banana = new Fruit(
+                    getRandomLengthName("Banana"), R.drawable.banana_pic);
+            fruitList.add(banana);
+            Fruit orange = new Fruit(
+                    getRandomLengthName("Orange"), R.drawable.orange_pic);
+            fruitList.add(orange);
+            Fruit watermelon = new Fruit(
+                    getRandomLengthName("Watermelon"), R.drawable.watermelon_pic);
+            fruitList.add(watermelon);
+            Fruit pear = new Fruit(
+                    getRandomLengthName("Pear"), R.drawable.pear_pic);
+            fruitList.add(pear);
+            Fruit grape = new Fruit(
+                    getRandomLengthName("Grape"), R.drawable.grape_pic);
+            fruitList.add(grape);
+            Fruit pineapple = new Fruit(
+                    getRandomLengthName("Pineapple"), R.drawable.pineapple_pic);
+            fruitList.add(pineapple);
+            Fruit strawberry = new Fruit(
+                    getRandomLengthName("Strawberry"), R.drawable.strawberry_pic);
+            fruitList.add(strawberry);
+            Fruit cherry = new Fruit(
+                    getRandomLengthName("Cherry"), R.drawable.cherry_pic);
+            fruitList.add(cherry);
+            Fruit mango = new Fruit(
+                    getRandomLengthName("Mango"), R.drawable.mango_pic);
+            fruitList.add(mango);
+        }
+    }
+
+    private String getRandomLengthName(String name) {
+        Random random = new Random();
+        int length = random.nextInt(20) + 1;
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            builder.append(name);
+        }
+        return builder.toString();
     }
 }
