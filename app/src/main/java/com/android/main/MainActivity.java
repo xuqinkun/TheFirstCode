02dda5c572ac732178180cd9acfcf0ba31aeafa6
xuqinkun <xu_qinkun@163.com>
2025-05-24 22:24:28 +0800

    《第一行代码》 第4章 探究碎片 动态添加碎片

--- app/src/main/java/com/android/main/MainActivity.java
+++ app/src/main/java/com/android/main/MainActivity.java
@@ -1,14 +1,39 @@
 package com.android.main;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.android.chapter04.AnotherRightFragment;
+import com.android.chapter04.RightFragment;
 
 public class MainActivity extends AppCompatActivity {
+    private boolean isAnother = false;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_layout);
+        Button button = findViewById(R.id.button_replace);
+        button.setOnClickListener(v -> {
+            if (isAnother) {
+                replaceFragment(new RightFragment());
+                isAnother = false;
+            } else {
+                isAnother = true;
+                replaceFragment(new AnotherRightFragment());
+            }
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.right_layout, fragment);
+        transaction.commit();
     }
 }
