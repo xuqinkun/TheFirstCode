37cc65ba620cdd71c4db9815c08f7d45936b58cb
xuqinkun <xu_qinkun@163.com>
2025-05-25 15:10:17 +0800

    《第一行代码》 第5章 详解广播机制 接收广播，监听网络连接状态

---
+++ app/src/main/java/com/android/chapter05/NetworkChangeReceiver.java
@@ -0,0 +1,19 @@
+package com.android.chapter05;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.widget.Toast;
+
+public class NetworkChangeReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        ConnectivityManager manager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        boolean isConnected = networkInfo != null && networkInfo.isConnectedOrConnecting();
+        Toast.makeText(context, "Network changes, connected:" + isConnected, Toast.LENGTH_SHORT).show();
+    }
+}
--- app/src/main/java/com/android/main/MainActivity.java
+++ app/src/main/java/com/android/main/MainActivity.java
@@ -1,14 +1,30 @@
 package com.android.main;
 
+import android.content.IntentFilter;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.chapter05.NetworkChangeReceiver;
+
 public class MainActivity extends AppCompatActivity {
+    private IntentFilter intentFilter;
+    private NetworkChangeReceiver networkChangeReceiver;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.news_main_layout);
+        setContentView(R.layout.blank_layout);
+        intentFilter = new IntentFilter();
+        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+        networkChangeReceiver = new NetworkChangeReceiver();
+        registerReceiver(networkChangeReceiver, intentFilter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(networkChangeReceiver);
     }
 }
