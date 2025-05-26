99ba2bf89bfb8dcf1d53048920d9b8e322683c25
xuqinkun <xu_qinkun@163.com>
2025-05-26 21:22:57 +0800

    《第一行代码》 第5章 详解广播机制 跨应用广播接收

---
+++ broadcastreceiver/src/main/java/com/android/broadcastreceiver/MainActivity.java
@@ -0,0 +1,20 @@
+package com.android.broadcastreceiver;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
+
+import com.android.main.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.blank_layout);
+    }
+
+}
