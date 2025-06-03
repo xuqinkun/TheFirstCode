6f5709b90e0983e81bf83753597276cdd8b8c3a2
xuqinkun <xu_qinkun@163.com>
2025-06-03 20:47:48 +0800

    《第一行代码》 第10章 探究服务 服务与活动绑定

--- app/src/main/java/com/android/chapter10/FirstService.java
+++ app/src/main/java/com/android/chapter10/FirstService.java
@@ -2,28 +2,48 @@ package com.android.chapter10;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 public class FirstService extends Service {
+    private static final String TAG = "FirstService";
+
+    private DownloadBinder binder = new DownloadBinder();
+
+    class DownloadBinder extends Binder {
+        public void startDownload() {
+            Log.d(TAG, "startDownload");
+        }
+
+        public int getProgress() {
+            Log.d(TAG, "getProgress");
+            return 0;
+        }
+    }
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        return binder;
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
+        Log.d(TAG, "onCreate");
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "onStartCommand");
         return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
     public void onDestroy() {
+        Log.d(TAG, "onDestroy");
         super.onDestroy();
     }
 }
--- app/src/main/java/com/android/chapter10/ServiceControlActivity.java
+++ app/src/main/java/com/android/chapter10/ServiceControlActivity.java
@@ -1,27 +1,57 @@
 package com.android.chapter10;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Button;
 
 import com.android.main.R;
 
 public class ServiceControlActivity extends AppCompatActivity {
+    private static final String TAG = "ServiceControlActivity";
+
+    private FirstService.DownloadBinder downloadBinder;
+
+    private final ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            Log.d(TAG, "onServiceConnected");
+            downloadBinder = (FirstService.DownloadBinder) service;
+            downloadBinder.startDownload();
+            downloadBinder.getProgress();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            Log.d(TAG, "onServiceDisconnected: ");
+        }
+    };
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_first_service);
         Button startService = (Button) findViewById(R.id.start_service);
         Button stopService = (Button) findViewById(R.id.stop_service);
+        Button bindService = (Button) findViewById(R.id.bind_service);
+        Button unbindService = (Button) findViewById(R.id.unbind_service);
         startService.setOnClickListener(v -> {
             startService(new Intent(this, FirstService.class));
         });
         stopService.setOnClickListener(v -> {
             stopService(new Intent(this, FirstService.class));
+        });
+        bindService.setOnClickListener(v -> {
+            Intent intent = new Intent(this, FirstService.class);
+            bindService(intent, connection, BIND_AUTO_CREATE);
+        });
+        unbindService.setOnClickListener(v -> {
+            unbindService(connection);
         });
     }
 }
